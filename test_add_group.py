be9e4e7a34cfa57a73dc8ac176df37415f38bb5b
Vito9285 <27vito@mail.ru>
2015-09-22 22:12:06 +0300

    Задание 2

--- test_add_group.py
+++ test_add_group.py
@@ -15,7 +15,7 @@ class test_add_group(unittest.TestCase):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
-    def test_add_group(self):
+    def test_add_empty_group(self):
         wd = self.open_home_page()
         wd.get("http://localhost/addressbook/")
         self.login(wd, username="admin", password="secret")
