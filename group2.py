7b5019928f8d4a3d175a44c9041fc57732ea6fc8
Vito9285 <27vito@mail.ru>
2015-09-22 23:35:42 +0300

    Выполнено задание 3

---
+++ group2.py
@@ -0,0 +1,9 @@
+__author__ = 'Vitaly'
+
+
+class Group2:
+
+    def __init__(self, firstname, lastname, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
--- test_create_contact.py
+++ test_create_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
+from group2 import Group2
 
 def is_alert_present(wd):
     try:
@@ -16,29 +16,50 @@ class test_create_contact(unittest.TestCase):
         self.wd.implicitly_wait(60)
     
     def test_test_create_contact(self):
-        success = True
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
-        wd.find_element_by_link_text("add new").click()
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_add_new_page(wd)
+        self.fill_contact_form(wd, Group2(firstname="nnnnn", lastname="lllll", email="nnnnn.lllll@mail.ru"))
+        self.enter(wd)
+
+    def test_test_create_empty_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_add_new_page(wd)
+        self.fill_contact_form(wd, Group2(firstname="", lastname="", email=""))
+        self.enter(wd)
+
+    def enter(self, wd):
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def fill_contact_form(self, wd, group2):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("nnnnn")
+        wd.find_element_by_name("firstname").send_keys(group2.firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("lllll")
+        wd.find_element_by_name("lastname").send_keys(group2.lastname)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("nnnnn.lllll@mail.ru")
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.assertTrue(success)
-    
+        wd.find_element_by_name("email").send_keys(group2.email)
+
+    def open_add_new_page(self, wd):
+        wd.find_element_by_link_text("add new").click()
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def tearDown(self):
         self.wd.quit()
 
